777f81106e8ac250c271e8661bb60bdcd909c36a
cashquest <info@cashquest.app>
2022-12-16 11:11:35 -0500

    Initial migrate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import { init } from "./utils/initGeometryLayer";
+import { useEffect, useRef } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTwitter,
+  faLinkedin,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
+// import { Canvas } from "react-three-fiber";
+// import { WebGLRenderer } from "three";
 
 function App() {
+  //const modelRef = useRef();
+
+  useEffect(() => {
+    return init();
+  }, []);
+
+  const words = [
+    "<3",
+    "pzazz",
+    "React",
+    "legendary grit",
+    "three.js",
+    "internet stuff",
+    "a keyboard",
+  ];
+  let index = 0;
+
+  function updateFooter() {
+    document.querySelector("footer").innerHTML =
+      "James, 2022 | Baked with " + words[index];
+    index = (index + 1) % words.length;
+  }
+
+  setInterval(updateFooter, 3000);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="hero">
+        <h1 className="title">I'm James</h1>
+        <p className="text">
+          I build software that positively impacts how we work, live and play.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="icons">
+          <a href="https://twitter.com/jgannon_" className="symbol">
+            <FontAwesomeIcon className="fa-2x" icon={faTwitter} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/jamesngannon/"
+            className="symbol"
+          >
+            <FontAwesomeIcon className="fa-2x" icon={faLinkedin} />
+          </a>
+          <a href="https://github.com/james-gannon" className="symbol">
+            <FontAwesomeIcon className="fa-2x" icon={faGithub} />
+          </a>
+          <a href="https://github.com/james-gannon" className="symbol">
+            <FontAwesomeIcon className="fa-2x" icon="fa-solid fa-file-user" />
+          </a>
+        </div>
+      </div>
+      <div className="projects">
+        <div className="projects-table">
+          <a href="https://jgannon.space" className="anchor">
+            <div className="projects-card-1">
+              <p className="title">Artwork</p>
+              <p className="info">Handcrafted art, cards and NFTs</p>
+            </div>
+          </a>
+          <a
+            href="https://www.notion.so/james-gannon/Internet-Field-Guide-dc3224fd47724f8e843857c8baab8766"
+            className="anchor"
+          >
+            <div className="projects-card-2">
+              <p className="title">Web3 Field Guide</p>
+              <p className="info">Internet research</p>
+            </div>
+          </a>
+          <a
+            href="https://github.com/james-gannon/book-shelf-vrf"
+            className="anchor"
+          >
+            <div className="projects-card-3">
+              <p className="title">BookShelf dApp</p>
+              <p className="info">Chainlink VRF-enabled Ethereum dApp</p>
+            </div>
+          </a>
+          <a
+            href="https://messari.io/report/hashing-it-out-with-hedera-hashgraph?referrer=all-research"
+            className="anchor"
+          >
+            <div className="projects-card-4">
+              <p className="title">Messari Research</p>
+              <p className="info">Hedera Hashgraph</p>
+            </div>
+          </a>
+          <a
+            href="https://www.notion.so/james-gannon/Hi-I-m-James-0e5910deed03493d9124406b0f40b7ac"
+            className="anchor"
+          >
+            <div className="projects-card-5">
+              <p className="title">Other Projects</p>
+              <p className="info">What I'm currently up to</p>
+            </div>
+          </a>
+        </div>
+      </div>
+      <footer></footer>
     </div>
   );
 }
---
+++ src/utils/initGeometryLayer.js
@@ -0,0 +1,112 @@
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+import gsap from "gsap";
+
+let camera, scene, renderer, clock;
+const loader = new GLTFLoader();
+
+// init
+
+export function init() {
+  scene = new THREE.Scene();
+
+  camera = new THREE.PerspectiveCamera(
+    70,
+    window.innerWidth / window.innerHeight,
+    0.01,
+    10
+  );
+  camera.position.z = 5;
+  camera.position.x = 0.2;
+  camera.position.y = -0.1;
+  camera.rotation.z = 1;
+
+  clock = new THREE.Clock();
+
+  const light = new THREE.AmbientLight(0xffffff, 11);
+  scene.add(light);
+
+  loader.load("/models/flashy_computer_setup.glb", (gltf) => {
+    /*mixer = new THREE.AnimationMixer(gltf.scene);
+
+    // Anims
+    animation_1 = gltf.animations[0]; // first animation
+    animation_2 = gltf.animations[1]; // first animation
+
+    mixer.clipAction(animation_1).play();*/
+
+    const model = gltf.scene;
+    model.scale.set(0.05, 0.05, 0.05);
+
+    gsap.to(camera.position, {
+      z: 1,
+      duration: 1,
+      ease: "back.out(1.7)",
+    });
+    gsap.to(camera.rotation, {
+      z: 0,
+      duration: 1,
+    });
+    gsap.to(model.rotation, {
+      y: 1,
+      duration: 1,
+      delay: 1,
+    });
+    gsap.to(model.rotation, {
+      y: Math.PI * 1.8,
+      x: 0.5,
+      duration: 2,
+      delay: 1,
+    });
+
+    gsap.to(model.scale, {
+      duration: 1,
+      delay: 1,
+      x: 0.015,
+      y: 0.015,
+      z: 0.015,
+    });
+
+    gsap.to(model.position, {
+      duration: 1,
+      delay: 1,
+      x: 0.1,
+      y: 0.5,
+    });
+
+    scene.add(model);
+
+    // Start the animation loop
+    // Start the animation loop
+    function animate() {
+      const time = clock.getElapsedTime();
+
+      model.position.y = Math.cos(time) * 0.02 + 0.4;
+      model.rotation.z = Math.cos(time) * 0.08;
+
+      // Tell the browser to call this function again on the next frame
+      requestAnimationFrame(animate);
+    }
+    animate();
+    // return model;
+  });
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setAnimationLoop(animation);
+  renderer.setClearColor(0xffffff, 1);
+  document.body.appendChild(renderer.domElement);
+
+  window.addEventListener("resize", () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  });
+}
+
+// animation
+
+function animation(time) {
+  renderer.render(scene, camera);
+}
